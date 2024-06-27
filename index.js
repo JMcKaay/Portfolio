88f38d395f20de3b563ae1375578a51ab3d9f7ec
JMcKaay <jamesmckay1995@gmail.com>
2024-06-27 16:38:41 +0100

    About me added

--- index.js
+++ index.js
@@ -423,4 +423,19 @@ init()
 
 
 
-//Layout Animation
+//Layout Animation (About)
+
+const observer = new IntersectionObserver ((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
