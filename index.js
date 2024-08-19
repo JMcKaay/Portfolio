3955374decad2365853578660e93c039e0301eec
James <162724220+JMcKaay@users.noreply.github.com>
2024-08-19 18:43:47 +0100

    Changes for Type sync to work on mobile devices and become more cross platform compatable

--- index.js
+++ index.js
@@ -398,54 +398,54 @@ const VND = ((document, window) => {
 
 //Main "Hi I am James"
 
-async function init () {
-  const node = document.querySelector("#type-text")
-  
-  await sleep(1000)
-  node.innerText = ""
-  await node.type('Hello,\u00A0')
-  
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+class TypeEffect {
+  constructor(element) {
+    this.element = element;
+    this.typeInterval = () => {
+      const randomMs = 100 * Math.random();
+      return randomMs < 50 ? 10 : randomMs;
+    };
+  }
+
+  async type(text) {
+    for (let character of text) {
+      this.element.innerText += character;
+      await sleep(this.typeInterval());
+    }
+  }
+
+  async delete(text) {
+    for (let character of text) {
+      this.element.innerText = this.element.innerText.slice(0, -1);
+      await sleep(this.typeInterval());
+    }
+  }
+}
+
+async function init() {
+  const node = document.querySelector("#type-text");
+  const typeEffect = new TypeEffect(node);
+
+  await sleep(1000);
+  node.innerText = "";
+  await typeEffect.type('Hello,\u00A0');
+
   while (true) {
-    await node.type('Welcome')
-    await sleep(2000)
-    await node.delete('Welcome')
-    await node.type('I\u00A0am\u00A0James')
-    await sleep(2000)
-    await node.delete('I\u00A0am\u00A0James')
+    await typeEffect.type('Welcome');
+    await sleep(2000);
+    await typeEffect.delete('Welcome');
+    await typeEffect.type('I\u00A0am\u00A0James');
+    await sleep(2000);
+    await typeEffect.delete('I\u00A0am\u00A0James');
   }
 }
 
-
-// Source code 🚩
-
-const sleep = time => new Promise(resolve => setTimeout(resolve, time))
-
-class TypeAsync extends HTMLSpanElement {
-  get typeInterval () {
-    const randomMs = 100 * Math.random()
-    return randomMs < 50 ? 10 : randomMs
-  }
-  
-  async type (text) {
-    for (let character of text) {
-      this.innerText += character
-      await sleep(this.typeInterval)
-    }
-  }
-  
-  async delete (text) {
-    for (let character of text) {
-      this.innerText = this.innerText.slice(0, this.innerText.length -1)
-      await sleep(this.typeInterval)
-    }
-  }
-}
-
-customElements.define('type-async', TypeAsync, { extends: 'span' })
-
-
-init()
-
+// Start the typing effect when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', init);
 
 
 
